bfdecb682c08799fc06963594b43770d87633bd3
melamit <151834564+melamit@users.noreply.github.com>
2023-12-11 14:47:56 +0000

    Fix patch.sh syntax errors

--- .utils/patch.sh
+++ .utils/patch.sh
@@ -2,14 +2,14 @@ cd $GITHUB_WORKSPACE/kernel_workspace/android-kernel/
 
 export KERNEL_VERSION=$(make kernelversion | cut -d. -f1,2)
 
-if[$($KERNEL_VERSION | cut -d. -f2) > 17 ]
-then 
+if [$KERNEL_VERSION | cut -d. -f2 > 17 ]
+then
     export KERNEL_PATCH_VERSION="4.14" 
 else
     export KERNEL_PATCH_VERSION="4.19"
 fi
 
-if[$($KERNEL_PATCH_VERSION) != undefined]
+if [$KERNEL_PATCH_VERSION != undefined]
 then 
         cd $GITHUB_WORKSPACE/kernel_workspace
         cd android-kernel

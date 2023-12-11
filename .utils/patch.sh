7d0565a838730731e94861f909e762d4c3c8b01a
melamit <151834564+melamit@users.noreply.github.com>
2023-12-11 15:01:02 +0000

    Fix syntax errors

--- .utils/patch.sh
+++ .utils/patch.sh
@@ -1,19 +1,24 @@
-cd $GITHUB_WORKSPACE/kernel_workspace/android-kernel/
+#!/bin/sh
+cd $GITHUB_WORKSPACE/kernel_workspace/android-kernel/ || exit
 
-export KERNEL_VERSION=$(make kernelversion | cut -d. -f1,2)
+# shellcheck disable=SC2046
+KERNEL_VERSION="$(make kernelversion | cut -d. -f1,2)"
+export KERNEL_VERSION
 
-if [$KERNEL_VERSION | cut -d. -f2 > 17 ]
+# shellcheck disable=SC2046
+if [ $(KERNEL_VERSION | cut -d. -f2) > 17 ]
 then
     export KERNEL_PATCH_VERSION="4.14" 
 else
     export KERNEL_PATCH_VERSION="4.19"
 fi
 
-if [$KERNEL_PATCH_VERSION != undefined]
+if [ $KERNEL_PATCH_VERSION != undefined ]
 then 
         cd $GITHUB_WORKSPACE/kernel_workspace
         cd android-kernel
         git apply $GITHUB_WORKSPACE/KernelSU/$KERNEL_PATCH_VERSION/KernelSU.patch
 else 
-    exit "Can't determine kernel patch version"
+    echo "Can't determine kernel patch version" >&2
+    exit 1
 fi
